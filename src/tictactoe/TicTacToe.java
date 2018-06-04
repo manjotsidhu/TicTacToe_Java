add5dcdeea3ad54e111f230060c9ca516c03f574
Manjot Sidhu <manjot.techie@gmail.com>
2018-06-04 15:12:29 +0530

    Validations for array size and values

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -5,8 +5,8 @@ import static tictactoe.Colors.*;
 
 /**
  * TODOs : 
- * 2 > dynamic size of tictactoe array (done) (testing)
- * 3 > Validations 
+ * 2 > dynamic size of tictactoe array (done) (implemented)
+ * 3 > Validations (done) (testing)
  * 4 > GUI 
  * 5 > Web interface
  *
@@ -31,13 +31,14 @@ public class TicTacToe {
     private int turn = u1;
     private boolean newLevel = true;
     private int pattern = 3;
+    private boolean restart = true;
 
     // Scanner
     Scanner scan = new Scanner(System.in);
 
     // Init game parameters
     public void init() {
-        System.out.print(MSS_GREEN + "Enter Length/Order: " + MSS_RESET);
+        System.out.print(MSS_GREEN + "Enter Size/Order: " + MSS_RESET);
 
         this.ticLength = scan.nextInt();
         // ticLength predefined
@@ -45,6 +46,15 @@ public class TicTacToe {
 
         ticArray = new int[ticLength][ticLength];
 
+    }
+    
+    // Validate array size/order values
+    public void validateStructure() {
+        if(this.ticLength < 3 || this.ticLength > 9) {
+            System.out.println(MSS_RED + "TicTacToe size cannot be less than 3 and more than 9" + MSS_RESET);
+            System.out.println(MSS_RED + "Please try again" + MSS_RESET);
+            init();
+        } 
     }
 
     // Render TicTacToe Structure
@@ -64,8 +74,8 @@ public class TicTacToe {
 
     }
 
-    // if Array isEmpty
-    public boolean isEmpty() {
+    // if Array isEmpty (not used for now)
+    /* public boolean isEmpty() {
         int empty = 0;
         for (int i = 0; i < this.ticLength; i++) {
             for (int j = 0; j < this.ticLength; j++) {
@@ -75,11 +85,22 @@ public class TicTacToe {
             }
         }
         return empty == 0;
+    } */
+    
+    // Validate Array indexes
+    public void validateInput(int i, int j) {
+        if(i > ticLength-1 || j > ticLength-1) {
+            System.out.println(MSS_RED + "Please input valid index number" + MSS_RESET);
+            restart = false;
+        } else if(i == 0 && j == 0) {
+            System.out.println(MSS_RED + "Invalid index, indexes starts from 1(inclusive)" + MSS_RESET);
+            restart = false;
+        }
     }
 
     // Ask user's input
     public void userInput() {
-        System.out.printf("%sUser %s, make your move: ", MSS_GREEN, turn);
+        System.out.printf("%sUser %s, make your move: %s", MSS_GREEN, turn, MSS_RESET);
     }
 
     // Implement User's turn in TicTacToe
@@ -191,11 +212,14 @@ public class TicTacToe {
         // Game starts with user 1
         turn = 1;
         init();
+        validateStructure();
         while (this.newLevel) {
             renderStructure();
             userInput();
             int input1 = scan.nextInt();
             int input2 = scan.nextInt();
+            validateInput(input1, input2);
+            if(!restart) { continue; } 
             setValue(input1, input2);
             logic(input1, input2);
             nextTurn();
