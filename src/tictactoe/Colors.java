d3f0192fa7a178bdca97c34228c4c6c595bf6567
Manjot Sidhu <manjot.techie@gmail.com>
2018-06-05 14:57:18 +0530

    Features! User-defined characters and custom colors

--- src/tictactoe/Colors.java
+++ src/tictactoe/Colors.java
@@ -10,23 +10,23 @@ package tictactoe;
 public class Colors {
 
     // for Text Codes
-    public static final String MSS_RESET = "\u001B[0m";
-    public static final String MSS_BLACK = "\u001B[30m";
-    public static final String MSS_RED = "\u001B[31m";
-    public static final String MSS_GREEN = "\u001B[32m";
-    public static final String MSS_YELLOW = "\u001B[33m";
-    public static final String MSS_BLUE = "\u001B[34m";
-    public static final String MSS_PURPLE = "\u001B[35m";
-    public static final String MSS_CYAN = "\u001B[36m";
-    public static final String MSS_WHITE = "\u001B[37m";
+    public static final String RESET = "\u001B[0m";
+    public static final String BLACK = "\u001B[30m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String PURPLE = "\u001B[35m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String WHITE = "\u001B[37m";
     // for text background
-    public static final String MSS_BLACK_BG = "\u001B[40m";
-    public static final String MSS_RED_BG = "\u001B[41m";
-    public static final String MSS_GREEN_BG = "\u001B[42m";
-    public static final String MSS_YELLOW_BG = "\u001B[43m";
-    public static final String MSS_BLUE_BG = "\u001B[44m";
-    public static final String MSS_PURPLE_BG = "\u001B[45m";
-    public static final String MSS_CYAN_BG = "\u001B[46m";
-    public static final String MSS_WHITE_BG = "\u001B[47m";
+    public static final String BLACK_BG = "\u001B[40m";
+    public static final String RED_BG = "\u001B[41m";
+    public static final String GREEN_BG = "\u001B[42m";
+    public static final String YELLOW_BG = "\u001B[43m";
+    public static final String BLUE_BG = "\u001B[44m";
+    public static final String PURPLE_BG = "\u001B[45m";
+    public static final String CYAN_BG = "\u001B[46m";
+    public static final String WHITE_BG = "\u001B[47m";
 
 }
--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -6,8 +6,8 @@ import static tictactoe.Colors.*;
 /**
  * TODOs : 
  * 1 > ticArray to char or to generics (done)
- * 2 > user defined characters and colors (wip)
- * 3 > more in-game color formattings
+ * 2 > user defined characters and colors (done)
+ * 3 > better in-game color formattings
  * 3 > GUI 
  * 5 > Web interface
  *
@@ -26,69 +26,112 @@ public class TicTacToe {
                                 {7,8,9,1,1,2}}; */
     private char[][] ticArray;
     
-    private char u1;
-    private char u2;
+    private char u1Char;
+    private char u2Char;
+    private String u1Color;
+    private String u2Color;
 
-    private char turn;
+    private char turnChar;
+    private String turnColor;
     private boolean newLevel = true;
-    private int pattern = 3;
+    private final int pattern = 3;
     private boolean restart = true;
 
     // Scanner
     Scanner scan = new Scanner(System.in);
-
-    // Init game parameters
-    public void init() {
-        
-        System.out.print(MSS_GREEN + "Enter Size/Order: " + MSS_RESET);
+    
+    // Set ticArray size
+    public void setSize() {
+        System.out.print(GREEN + "Enter Size/Order: " + RESET);
 
         this.ticSize = scan.nextInt();
         // ticSize predefined
         // this.ticSize = 6;
 
         ticArray = new char[ticSize][ticSize];
+    }
+    
+    // Set User charater and color properties
+    public void setUserProp() {
+        System.out.print(GREEN + "User 1, enter your character: " + RESET);
+        this.u1Char = scan.next(".").charAt(0);
         
-        System.out.print(MSS_GREEN + "User 1, select your character: " + MSS_RESET);
-        this.u1 = scan.next(".").charAt(0);
+        System.out.print(GREEN + "User 2, enter your character: " + RESET);
+        this.u2Char = scan.next(".").charAt(0);
         
-        System.out.print(MSS_GREEN + "User 2, select your character: " + MSS_RESET);
-        this.u2 = scan.next(".").charAt(0);
+        // u1Char and u2Char predefined
+        // this.u1Char = 'X';
+        // this.u2Char = 'O';
         
-        // u1 and u2 predefined
-        // this.u1 = 'X';
-        // this.u2 = 'O';
+        System.out.print(GREEN + "User 1, enter your color: " + RESET);
+        this.u1Color = scan.next();
+                
+        System.out.print(GREEN + "User 2, enter your color: " + RESET);
+        this.u2Color = scan.next();
+    }
+
+    // Custom User Colors
+    public void userColors() {
+        switch(u1Color.trim().toUpperCase()) {
+            case "BLACK" : u1Color = BLACK; break;
+            case "RED" : u1Color = RED; break;
+            case "GREEN" : u1Color = GREEN; break;
+            case "YELLOW" : u1Color = GREEN; break;
+            case "BLUE" : u1Color = BLUE; break;
+            case "PURPLE" : u1Color = PURPLE; break;
+            case "CYAN" : u1Color = CYAN; break;
+            case "WHITE" : u1Color = WHITE; break;
+        }
+        switch(u2Color.trim().toUpperCase()) {
+            case "BLACK" : u2Color = BLACK; break;
+            case "RED" : u2Color = RED; break;
+            case "GREEN" : u2Color = GREEN; break;
+            case "YELLOW" : u2Color = GREEN; break;
+            case "BLUE" : u2Color = BLUE; break;
+            case "PURPLE" : u2Color = PURPLE; break;
+            case "CYAN" : u2Color = CYAN; break;
+            case "WHITE" : u2Color = WHITE; break;
+        }
+    }
+    
+    // Init in-game parameters
+    public void init() {
+        setSize();
+        setUserProp();
+        userColors();
         
-        // Game starts with user 1
-        this.turn = u1;
+        // Game starts with first user
+        this.turnChar = u1Char;
+        this.turnColor = u1Color;
     }
     
     // Validate array size/order values
     public void validateStructure() {
         if(this.ticSize < 3 || this.ticSize > 9) {
-            System.out.println(MSS_RED + "TicTacToe size cannot be less than 3 and more than 9" + MSS_RESET);
-            System.out.println(MSS_RED + "Please try again" + MSS_RESET);
-            init();
+            System.out.println(RED + "TicTacToe size cannot be less than 3 and more than 9" + RESET);
+            System.out.println(RED + "Please try again" + RESET);
+            setSize();
         } 
     }
 
     // Render TicTacToe Structure
     public void renderStructure() {
-        System.out.print(new String(new char[this.ticSize]).replace("\0", MSS_GREEN_BG + MSS_BLACK + "+---"));
-        System.out.println("+" + MSS_RESET);
+        System.out.print(new String(new char[this.ticSize]).replace("\0", GREEN_BG + BLACK + "+---"));
+        System.out.println("+" + RESET);
 
         for (int i = 0; i < this.ticSize; i++) {
             for (int j = 0; j < this.ticSize; j++) {
                 if(ticArray[i][j] == 0) {
-                    System.out.print(MSS_GREEN_BG + MSS_BLACK + "| " + "  ");
+                    System.out.print(GREEN_BG + BLACK + "| " + "  ");
                 } else {
-                    System.out.print(MSS_GREEN_BG + MSS_BLACK + "| " + ticArray[i][j] + " ");
+                    System.out.print(GREEN_BG + BLACK + "| " + ticArray[i][j] + " ");
                 }
             }
-            System.out.println("|" + MSS_RESET);
+            System.out.println("|" + RESET);
         }
 
-        System.out.print(new String(new char[this.ticSize]).replace("\0", MSS_GREEN_BG + MSS_BLACK + "+---"));
-        System.out.println("+" + MSS_RESET);
+        System.out.print(new String(new char[this.ticSize]).replace("\0", GREEN_BG + BLACK + "+---"));
+        System.out.println("+" + RESET);
 
     }
 
@@ -108,14 +151,14 @@ public class TicTacToe {
     // Validate Array indexes
     public void validateInput(int i, int j) {
         if(i > ticSize-1 || j > ticSize-1) {
-            System.out.println(MSS_RED + "Please input valid index number" + MSS_RESET);
+            System.out.println(RED + "Please input valid index number" + RESET);
             this.restart = false;
         } // foreground fix :( to not start indexes from 0  
         /*else if(i == 0 && j == 0) {
-            System.out.println(MSS_RED + "Invalid index, indexes starts from 1(inclusive)" + MSS_RESET);
+            System.out.println(RED + "Invalid index, indexes starts from 1(inclusive)" + RESET);
             this.restart = false;
         }*/ else if(ticArray[i][j] != 0) {
-            System.out.println(MSS_RED + i + " " + j + " index already has " + ticArray[i][j] + " value." + MSS_RESET);
+            System.out.println(RED + i + " " + j + " index already has " + ticArray[i][j] + " value." + RESET);
             this.restart = false;
         } else {
             this.restart = true;
@@ -124,17 +167,18 @@ public class TicTacToe {
 
     // Ask user's input
     public void userInput() {
-        System.out.printf("%sUser %s, make your move: %s", MSS_GREEN, this.turn, MSS_RESET);
+        System.out.printf("%sUser %s, make your move: %s", turnColor, this.turnChar, RESET);
     }
 
     // Implement User's turn in TicTacToe
     public void setValue(int i, int j) {
-        ticArray[i][j] = this.turn;
+        ticArray[i][j] = this.turnChar;
     }
 
-    // Set next user's turn accordingly
+    // Set next user's turn and color accordingly
     public void nextTurn() {
-        this.turn = (this.turn == this.u1) ? this.u2 : this.u1;
+        this.turnChar = (this.turnChar == this.u1Char) ? this.u2Char : this.u1Char;
+        this.turnColor = (this.turnColor == this.u1Color) ? this.u2Color : this.u1Color;
     }
 
     // Game Logic 
@@ -144,7 +188,7 @@ public class TicTacToe {
             result6 = 0;
 
         for (int i = 0; i < this.ticSize; i++) {
-            if (ticArray[i][i2] == this.turn) {
+            if (ticArray[i][i2] == this.turnChar) {
                 result1++;
                 if (result1 == this.pattern) {
                     this.newLevel = false;
@@ -155,7 +199,7 @@ public class TicTacToe {
                 result1 = 0;
             }
 
-            if (ticArray[i1][i] == this.turn) {
+            if (ticArray[i1][i] == this.turnChar) {
                 result2++;
                 if (result2 == this.pattern) {
                     this.newLevel = false;
@@ -166,7 +210,7 @@ public class TicTacToe {
                 result2 = 0;
             }
 
-            if (ticArray[i][i] == this.turn) {
+            if (ticArray[i][i] == this.turnChar) {
                 result3++;
                 if (result3 == this.pattern) {
                     this.newLevel = false;
@@ -179,7 +223,7 @@ public class TicTacToe {
 
             for (int j = 0; j < this.ticSize; j++) {
                 if (i + j == this.ticSize - 1) {
-                    if (ticArray[i][j] == this.turn) {
+                    if (ticArray[i][j] == this.turnChar) {
                         result4++;
                         if (result4 == this.pattern) {
                             this.newLevel = false;
@@ -194,7 +238,7 @@ public class TicTacToe {
 
             for (int j = 0; j < this.ticSize; j++) {
                 if (i + j == i1 + i2) {
-                    if (ticArray[i][j] == this.turn) {
+                    if (ticArray[i][j] == this.turnChar) {
                         result5++;
                         if (result5 == this.pattern) {
                             this.newLevel = false;
@@ -209,7 +253,7 @@ public class TicTacToe {
 
             for (int j = 0; j < this.ticSize; j++) {
                 if (i + j == Math.abs(i1 - i2)) {
-                    if (ticArray[i][j] == this.turn) {
+                    if (ticArray[i][j] == this.turnChar) {
                         result6++;
                         if (result6 == this.pattern) {
                             this.newLevel = false;
@@ -227,8 +271,8 @@ public class TicTacToe {
     // Results
     public void result() {
         renderStructure();
-        System.out.printf("%s%sUser %s won the game.\n", MSS_RED_BG, MSS_WHITE, turn);
-        System.out.println(MSS_RED_BG + MSS_WHITE + "Thanks For Playing." + MSS_RESET);
+        System.out.printf("%s%sUser %s won the game.\n", RED_BG, WHITE, turnChar);
+        System.out.println(RED_BG + WHITE + "Thanks For Playing." + RESET);
     }
 
     // Start main game
